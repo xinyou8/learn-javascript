6b2b2554175f280b27452f900e6f4848f2062d5c
xinyou8 <289831485@qq.com>
2017-07-04 17:41:54 +0800

    Update file_server.js
    process.argv是以空格来分的

--- samples/node/http/file_server.js
+++ samples/node/http/file_server.js
@@ -8,14 +8,17 @@ var
     path = require('path'),
     http = require('http');
 
-var root = path.resolve(process.argv[2] || '.');
+var root = path.resolve(process.argv[2] || '.');// 这里控制台输入js文件后面需跟空格再跟路径。路径跟js文件位于同一磁盘下，所以磁盘不用输入
+                                                // eg: node demo.js http/args
+                                                //demo.js 位于D:/xx/xx/..
+                                                //index.html(默认文件)位于D:/http/args
 
 console.log('Static root dir: ' + root);
 
 var server = http.createServer(function (request, response) {
     var
-        pathname = url.parse(request.url).pathname, // '/static/bootstrap.css'
-        filepath = path.join(root, pathname); // '/srv/www/static/bootstrap.css'
+        pathname = url.parse(request.url).pathname, // 默认 index.html
+        filepath = path.join(root, pathname); // 'D:/http/args/index.html'
     fs.stat(filepath, function (err, stats) {
         if (!err && stats.isFile()) {
             console.log('200 ' + request.url);
